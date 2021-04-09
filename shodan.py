09c59e5b5ac6ab1e3da3d3425590a23d795a6eae
Joy Ghosh <36255129+JoyGhoshs@users.noreply.github.com>
2021-04-09 02:27:28 -0400

    Add files via upload

---
+++ shodan.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+__author__      = "Joy Ghosh"
+__Ver__         = "V 1.0"
+__copyright__   = "Copyright 2021, SYSTEM00 SECURITY"
+from colorama import Fore, Back, Style
+import requests
+import argparse
+from json2html import *
+##########################
+api_key="UPmNGzp5HfQwsNY5BFCIuPLFUSPAa5E6"
+##########################
+api_url='https://api.shodan.io/shodan/host/search?key='+api_key+'&query='
+##########################
+def logo():
+    print(Fore.RED+"""
+
+█▀▀ █▀ █░█ █▀█ █▀▄ ▄▀█ █▄░█
+█▄█ ▄█ █▀█ █▄█ █▄▀ █▀█ █░▀█
+---------------------------
+    Gui Shodans result
+---------------------------
+
+    """+Fore.WHITE)
+def shodan_result(query):
+    request=requests.get(api_url+query)
+    result=request.text
+    html=json2html.convert(json = result)
+    out=open(query+'.html', "w")
+    out.write(html)
+    out.close()
+    print(Fore.GREEN+"[X] File Saved as : "+query+'.html'+Fore.WHITE)
+logo()
+try:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--query", help="Enter Your Shodan Query ", type=str)
+    args = parser.parse_args()
+    shodan_result(args.query)
+except TypeError:
+  print("Type -h to see all the options")
+except:
+  exit()
